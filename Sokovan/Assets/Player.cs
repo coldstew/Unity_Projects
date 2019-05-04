57520de4c2292f50c90aad254b4a64db51c6997e
coldstew <coldstew@gmail.com>
2019-05-04 21:24:27 +0200

    Sokovan initial

---
+++ Sokovan/Assets/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10f;
+    private Rigidbody playerRigidbody; // it's private to prevent any differences
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // A <-            -> D
+        // -1.0      0     +1.0
+        
+        float inputX = Input.GetAxis("Horizontal");
+        
+        float inputZ = Input.GetAxis("Vertical");
+
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        
+        velocity = velocity * speed;
+
+        velocity.y = fallSpeed;
+
+        playerRigidbody.velocity = velocity;
+
+        /* if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidbody.AddForce(0,0,speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.AddForce(-speed,0,0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidbody.AddForce(0,0,-speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.AddForce(speed,0,0);
+        } */
+    }
+}
